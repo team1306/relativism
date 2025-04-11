55535f5ec0056b6901f9264961ac78252a77d549
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2025-04-10 21:56:47 -0500

    add zoom stuff

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:relativism/widgets/toolbar/toolbar.dart';
-import 'package:relativism/widgets/variablebar.dart';
 
 import '../widgets/field.dart';
 
@@ -18,15 +17,27 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       appBar: Toolbar(),
-      body: Row(
-        children: [
-          Flexible(flex: 0, child: ConstrainedBox(constraints: BoxConstraints(maxWidth: 324), child: VariableBar())),
-          Padding(padding: EdgeInsets.all(4)),
-          VerticalDivider(),
-          Padding(padding: EdgeInsets.all(8)),
-          Flexible(flex: 1, child: Field()),
-          Padding(padding: EdgeInsets.all(8))
-        ]
+      body: Center(
+        child: 
+          ZoomableImageWithObjects(
+            imageUrl: 'images/2025_Field.png',
+            customCenter: const Offset(-10, 530), // Set your desired (0,0) point
+            objectsBuilder: (scale) => [
+              // Draw objects relative to the custom center
+              Positioned(
+                left: 0 * scale, // X coordinate from custom (0,0)
+                top: 0 * scale, // Y coordinate from custom (0,0)
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
       ),
     );
   }
--- lib/widgets/field.dart
+++ lib/widgets/field.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
 
-class Field extends StatefulWidget {
-  const Field({super.key});
+class ZoomableImageWithObjects extends StatefulWidget {
+  final String imageUrl;
+  final List<Widget> Function(double scale) objectsBuilder;
+  final Offset customCenter;
+
+  const ZoomableImageWithObjects({
+    super.key,
+    required this.imageUrl,
+    required this.objectsBuilder,
+    required this.customCenter,
+  });
 
   @override
-  State<StatefulWidget> createState() => FieldState();
+  State<ZoomableImageWithObjects> createState() => _ZoomableImageWithObjectsState();
 }
 
-class FieldState extends State<Field> {
+class _ZoomableImageWithObjectsState extends State<ZoomableImageWithObjects> {
+  double? imageWidth;
+  double? imageHeight;
+  ImageStream? _imageStream;
+  ImageStreamListener? _imageListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImage();
+  }
+
+  void _loadImage() {
+    final image = Image.asset(widget.imageUrl);
+    _imageStream = image.image.resolve(const ImageConfiguration());
+    _imageListener = ImageStreamListener((ImageInfo info, _) {
+      setState(() {
+        imageWidth = info.image.width.toDouble();
+        imageHeight = info.image.height.toDouble();
+      });
+    });
+    _imageStream!.addListener(_imageListener!);
+  }
+
+  @override
+  void dispose() {
+    _imageStream?.removeListener(_imageListener!);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset("images/2025_Field.png", fit: BoxFit.contain);
+    if (imageWidth == null || imageHeight == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return InteractiveViewer(
+      boundaryMargin: const EdgeInsets.all(double.infinity),
+      minScale: 0.1,
+      maxScale: 10.0,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          // Apply transform to shift the custom center to (0,0)
+          Transform.translate(
+            offset: Offset(-widget.customCenter.dx, -widget.customCenter.dy),
+            child: Image.asset(
+              widget.imageUrl,
+              width: imageWidth,
+              height: imageHeight,
+              fit: BoxFit.contain,
+            ),
+          ),
+          // Objects are positioned relative to the custom (0,0)
+          ...widget.objectsBuilder(1.0).map((obj) => obj),
+        ],
+      ),
+    );
   }
 }
